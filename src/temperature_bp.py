9c3b5d52c51885e62355f65342a3f3f4fed1cb84
Zoltan Varju <zoltan.varju@gmail.com>
2022-07-14 17:04:50 +0200

    Initial commit

---
+++ src/temperature_bp.py
@@ -0,0 +1,49 @@
+import os
+
+import h3
+import xarray as xr
+
+root_folder = "data/temp_data"
+dirs = [os.path.join(root_folder, d) for d in os.listdir(root_folder) if os.path.isdir(os.path.join(root_folder, d))]
+
+
+def is_within_bounding_box(lat, long):
+    if 47.392134 < lat < 47.601216 and 18.936234 < long < 19.250031:
+        return True
+    else:
+        return False
+
+
+latlong_temp = {}
+for inpath in dirs:
+    # TODO: take all data and calculate avg tmp for the month
+    # geodetic_tx.nc -> latitude_tx, longitude_tx
+    geodetic = xr.open_dataset(filename_or_obj=os.path.join(inpath, "geodetic_tx.nc"), engine='netcdf4')
+    lat = geodetic.data_vars["latitude_tx"].to_numpy().flatten()
+    long = geodetic.data_vars["longitude_tx"].to_numpy().flatten()
+    # met_tx.nc -> temperature_tx
+    met_tx = xr.open_dataset(filename_or_obj=os.path.join(inpath, "met_tx.nc"), engine='netcdf4')
+    temp = met_tx.data_vars["temperature_tx"].to_numpy().flatten()
+    # LST_ancillary_ds.nc -> NDVI (empyt :()
+    lst = xr.open_dataset(filename_or_obj=os.path.join(inpath, "LST_ancillary_ds.nc"), engine='netcdf4')
+    ndvi = lst.data_vars["NDVI"].to_numpy().flatten()
+    temp_data = zip(lat, long, temp)
+    temp_data = (e for e in temp_data if is_within_bounding_box(e[0], e[1]))
+    for e in temp_data:
+        k = (e[0], e[1])
+        if latlong_temp.get(k, False):
+            latlong_temp[k] = (latlong_temp[k] + e[2]) / 2
+        else:
+            latlong_temp[k] = e[2]
+
+with open("data/temp_budapest.tsv", "w") as outfile:
+    h = "lat\tlong\tkelvin\tl6\tl7\tl8\n"
+    outfile.write(h)
+    for k,v in latlong_temp.items():
+        l6 = h3.geo_to_h3(k[0], k[1], 6)
+        l7 = h3.geo_to_h3(k[0], k[1], 7)
+        l8 = h3.geo_to_h3(k[0], k[1], 8)
+        o = str(k[0]) + "\t" + str(k[1]) + "\t" + str(v) + "\t" + l6 + "\t" + l7 + "\t" + l8 + "\n"
+        outfile.write(o)
+
+print("we have data!!!!!!!!"*100)
---
+++ src/viz_temp.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pydeck as pdk
+
+df = pd.read_csv("data/temp_budapest.tsv", sep="\t")
+
+levels = ["l6", "l7", "l8"]
+
+for level in levels:
+    print(level)
+    layer = pdk.Layer(
+        "H3HexagonLayer",
+        df,
+        get_hexagon=level,
+        auto_highlight=True,
+        elevation_scale=15,
+        pickable=True,
+        elevation_range=[min(df["kelvin"]), max(df["kelvin"])],
+        extruded=True,
+        coverage=0.8,
+        opacity=0.01,
+        get_fill_color="[0, 255, kelvin]"
+    )
+
+    view_state = pdk.ViewState(latitude=47.500000, longitude=19.040236,
+                               zoom=10, bearing=0, pitch=35)
+    r = pdk.Deck(layers=[layer],
+                 initial_view_state=view_state,
+                 tooltip={"text": "temperature: {kelvin}"})
+    r.to_html(f"vizs/temperature_{level}.html")
